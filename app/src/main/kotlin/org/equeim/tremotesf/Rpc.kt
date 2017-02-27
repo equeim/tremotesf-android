aa23fbd8ffd8796fdbb44d22292a9a5a145236cd
Alexey Rochev <equeim@gmail.com>
2017-02-27 17:43:27 +0300

    Return type is not nullable

--- app/src/main/kotlin/org/equeim/tremotesf/Rpc.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/Rpc.kt
@@ -218,7 +218,7 @@ object Rpc {
     private lateinit var password: String
 
     private val responseDeserializer = object : ResponseDeserializable<JsonObject> {
-        override fun deserialize(content: String): JsonObject? {
+        override fun deserialize(content: String): JsonObject {
             return gson.fromJson(content, JsonObject::class.java)
         }
     }
