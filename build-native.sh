ffbf5e57f98ca8ed98c7cbc35a1e27df7a9b0251
Alexey Rochev <equeim@gmail.com>
2018-07-04 20:12:26 +0300

    Remove QtAndroid-bundled.jar check

--- build-native.sh
+++ build-native.sh
@@ -18,10 +18,4 @@ export ANDROID_ARCH=x86
 _build || exit 1
 
 _QT="$(realpath $_DIR/3rdparty/qt/install-$ANDROID_ARCH)"
-_BUNDLED="$_QT/jar/QtAndroid-bundled.jar"
-_LIBS="$_DIR/app/libs/"
-if [ -f "$_BUNDLED" ]; then
-    cp "$_BUNDLED" "$_LIBS" || exit 1
-else
-    cp "$_QT/jar/QtAndroid.jar" "$_LIBS" || exit 1
-fi
+cp "$_QT/jar/QtAndroid.jar" "$_DIR/app/libs/" || exit 1
