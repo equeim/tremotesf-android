d3a4f59e55eedd44cac2a2c86bfaa231f9d049bc
Alexey Rochev <equeim@gmail.com>
2019-06-09 01:11:40 +0300

    Build native libraries for 64-bit architectures too

--- 3rdparty/openssl/build.sh
+++ 3rdparty/openssl/build.sh
@@ -9,6 +9,12 @@ case "$ANDROID_ARCH" in
     "x86")
         export _TARGET="android-x86"
     ;;
+    "arm64-v8a")
+        export _TARGET="android-arm64"
+    ;;
+    "x86_64")
+        export _TARGET="android-x86_64"
+    ;;
 esac
 
 _BUILD_DIR="$_DIR/build-$ANDROID_ARCH"
@@ -18,7 +24,7 @@ cd "$_BUILD_DIR" || exit 1
 _PREFIX="$_DIR/install-$ANDROID_ARCH"
 export ANDROID_NDK="$ANDROID_NDK_ROOT"
 export PATH="$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
-../openssl/Configure $_TARGET shared no-ssl3 no-comp no-hw no-engine --prefix="$_PREFIX" -D__ANDROID_API__=16 || exit 1
+../openssl/Configure $_TARGET shared no-ssl3 no-comp no-hw no-engine --prefix="$_PREFIX" -D__ANDROID_API__=$ANDROID_API || exit 1
 
 make build_libs $MAKEOPTS || exit 1
 make install_dev $MAKEOPTS || exit 1
--- build-native.sh
+++ build-native.sh
@@ -11,11 +11,21 @@ _DIR="$(realpath $(dirname $0))"
 
 export MAKEOPTS="$@"
 
+export ANDROID_API=16
+
 export ANDROID_ARCH=armeabi-v7a
 _build || exit 1
 
 export ANDROID_ARCH=x86
 _build || exit 1
 
-_QT="$(realpath $_DIR/3rdparty/qt/install-$ANDROID_ARCH)"
+export ANDROID_API=21
+
+export ANDROID_ARCH=arm64-v8a
+_build || exit 1
+
+export ANDROID_ARCH=x86_64
+_build || exit 1
+
+_QT="$(realpath $_DIR/3rdparty/qt/install-armeabi-v7a)"
 cp "$_QT/jar/QtAndroid.jar" "$_DIR/app/libs/" || exit 1
