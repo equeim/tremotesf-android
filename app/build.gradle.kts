49bc2dd9fb2d221b6f768d0d943c87fdf539f4cb
Alexey Rochev <equeim@gmail.com>
2021-06-11 23:47:02 +0300

    Revert "Migrate from jcenter to jitpack for fastscroll library"
    This reverts commit f157655e7b281579bad49e898087c33ab24b1327.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,11 @@ android {
 repositories {
     mavenCentral()
     google()
-    maven("https://jitpack.io")
+    jcenter {
+        content {
+            includeModule("com.l4digital.fastscroll", "fastscroll")
+        }
+    }
 }
 
 dependencies {
@@ -118,7 +122,7 @@ dependencies {
 
     implementation("com.google.android.material:material:${Versions.material}")
 
-    implementation("com.github.L4Digital:FastScroll:${Versions.fastscroll}")
+    implementation("com.l4digital.fastscroll:fastscroll:${Versions.fastscroll}")
 
     implementation("com.jakewharton.timber:timber:${Versions.timber}")
 
