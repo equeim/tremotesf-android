dda970355cd1083d751124515c2300ffb433a003
Alexey Rochev <equeim@gmail.com>
2021-06-13 23:21:20 +0300

    Use WindowCompat.setDecorFitsSystemWindows()

--- app/src/main/kotlin/org/equeim/tremotesf/ui/NavigationActivity.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/NavigationActivity.kt
@@ -40,6 +40,7 @@ import androidx.appcompat.view.ActionMode
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.marginLeft
 import androidx.core.view.marginRight
@@ -128,17 +129,7 @@ class NavigationActivity : AppCompatActivity(), NavControllerProvider {
 
         setContentView(binding.root)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-        } else {
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = (
-                    window.decorView.systemUiVisibility or
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
-        }
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.apply {
