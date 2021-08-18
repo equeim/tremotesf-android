7f7bab634542aeec5df1b89dbfb2490a016cf6dc
Alexey Rochev <equeim@gmail.com>
2021-08-19 02:29:31 +0300

    Improve showKeyboard() logic

--- app/src/main/kotlin/org/equeim/tremotesf/ui/utils/Keyboard.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/utils/Keyboard.kt
@@ -1,11 +1,15 @@
 package org.equeim.tremotesf.ui.utils
 
 import android.app.Activity
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.content.getSystemService
-import androidx.core.view.postDelayed
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import timber.log.Timber
 
 fun Activity.hideKeyboard() {
     currentFocus?.let { focus ->
@@ -17,17 +21,29 @@ fun Fragment.hideKeyboard() {
     activity?.hideKeyboard()
 }
 
+private const val IMM_IS_ACTIVE_CHECK_INTERVAL_MS = 50L
+
 fun EditText.showKeyboard() {
-    context.getSystemService<InputMethodManager>()?.let { imm ->
+    val imm = context.getSystemService<InputMethodManager>() ?: return
+    val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+    val job = lifecycleOwner.lifecycleScope.launchWhenStarted {
         if (requestFocus()) {
-            // If you call showSoftInput() right after requestFocus()
-            // it may sometimes fail. So add a 50ms delay
-            postDelayed(50) {
-                // Check if we are still focused
-                if (isFocused) {
-                    imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
-                }
+            while (isFocused && !imm.isActive(this@showKeyboard)) {
+                delay(IMM_IS_ACTIVE_CHECK_INTERVAL_MS)
             }
+            if (isFocused) {
+                imm.showSoftInput(this@showKeyboard, InputMethodManager.SHOW_IMPLICIT)
+            }
+        } else {
+            Timber.w("showKeyboard: failed to request focus")
         }
     }
+    val listener = object : View.OnAttachStateChangeListener {
+        override fun onViewAttachedToWindow(v: View?) = Unit
+        override fun onViewDetachedFromWindow(v: View?) = job.cancel()
+    }
+    job.invokeOnCompletion {
+        removeOnAttachStateChangeListener(listener)
+    }
+    addOnAttachStateChangeListener(listener)
 }
