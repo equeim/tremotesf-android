5ec86a90283e6834f71d7a143a9133174b477819
Alexey Rochev <equeim@gmail.com>
2021-09-17 23:45:42 +0300

    Remove unused resource and import

--- app/src/main/kotlin/org/equeim/tremotesf/ui/utils/Extensions.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/utils/Extensions.kt
@@ -25,7 +25,6 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.text.Editable
 import android.view.View
-import android.view.ViewGroup
 import android.view.ViewParent
 import android.widget.CheckBox
 import android.widget.EditText
