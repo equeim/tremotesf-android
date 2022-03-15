b125a809202ca48617f66931de32481d597db1b5
Alexey Rochev <equeim@gmail.com>
2022-03-15 23:40:11 +0300

    Update coroutines to 1.6.0

--- app/src/main/kotlin/org/equeim/tremotesf/ui/utils/Flow.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/utils/Flow.kt
@@ -10,10 +10,10 @@ import kotlinx.coroutines.launch
 fun <T> Flow<T>.launchAndCollectWhenStarted(lifecycleOwner: LifecycleOwner) =
     lifecycleOwner.lifecycleScope.launch { lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) { collect() } }
 
-inline fun <T> Flow<T>.launchAndCollectWhenStarted(
+fun <T> Flow<T>.launchAndCollectWhenStarted(
     lifecycleOwner: LifecycleOwner,
-    crossinline action: suspend (value: T) -> Unit
-) = lifecycleOwner.lifecycleScope.launch { lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) { collect(action) } }
+    collector: FlowCollector<T>
+) = lifecycleOwner.lifecycleScope.launch { lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) { collect(collector) } }
 
 inline fun MutableStateFlow<Boolean>.handleAndReset(crossinline action: suspend () -> Unit) =
     filter { it }.onEach {
--- torrentfile/src/test/kotlin/org/equeim/tremotesf/torrentfile/TestDispatchers.kt
+++ torrentfile/src/test/kotlin/org/equeim/tremotesf/torrentfile/TestDispatchers.kt
@@ -2,10 +2,11 @@ package org.equeim.tremotesf.torrentfile
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestDispatcher
 import org.equeim.tremotesf.common.TremotesfDispatchers
 
-@ExperimentalCoroutinesApi
-class TestDispatchers constructor(dispatcher: TestCoroutineDispatcher) : TremotesfDispatchers {
+@OptIn(ExperimentalCoroutinesApi::class)
+class TestDispatchers constructor(dispatcher: TestDispatcher) : TremotesfDispatchers {
     override val Default = dispatcher
     override val IO = dispatcher
     override val Main = dispatcher
