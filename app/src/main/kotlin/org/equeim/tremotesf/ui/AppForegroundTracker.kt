c09eeb8116417ccb7ce0e07e1195732f0cb95979
Alexey Rochev <equeim@gmail.com>
2022-04-10 00:43:20 +0300

    Handle multiple activities in AppForegroundTracker

--- app/src/main/kotlin/org/equeim/tremotesf/ui/AppForegroundTracker.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/AppForegroundTracker.kt
@@ -19,29 +19,100 @@
 
 package org.equeim.tremotesf.ui
 
-import kotlinx.coroutines.DelicateCoroutinesApi
+import android.app.Activity
+import androidx.activity.ComponentActivity
+import androidx.annotation.MainThread
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleService
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.plus
 import timber.log.Timber
+import java.lang.ref.WeakReference
 
 object AppForegroundTracker {
-    val hasStartedActivity = MutableStateFlow(false)
-    val foregroundServiceStarted = MutableStateFlow(false)
+    private val scope = CoroutineScope(SupervisorJob())
 
-    @OptIn(DelicateCoroutinesApi::class)
-    private val scope = GlobalScope + Dispatchers.Unconfined
+    private val startedActivities = mutableSetOf<Activity>()
+    private val changingConfigurationActivities = mutableSetOf<WeakReference<Activity>>()
+    private val startedActivitiesCount = MutableStateFlow(0)
+
+    @MainThread
+    fun registerActivity(activity: ComponentActivity) {
+        Timber.d("registerActivity() called with: activity = $activity")
+        activity.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onStart(owner: LifecycleOwner) {
+                Timber.d("onStart() called with: owner = $owner")
+                if (!startedActivities.add(activity)) {
+                    Timber.e("onStart: activity is already in startedActivities")
+                }
+                changingConfigurationActivities.clear()
+                updateStartedActivitiesCount()
+            }
+
+            override fun onStop(owner: LifecycleOwner) {
+                Timber.d("onStop() called with: owner = $owner")
+                if (!startedActivities.remove(activity)) {
+                    Timber.e("onStop: activity is not in startedActivities")
+                }
+                if (activity.isChangingConfigurations) {
+                    Timber.i("onStop: activity is changing configuration")
+                    changingConfigurationActivities.add(WeakReference(activity))
+                }
+                updateStartedActivitiesCount()
+            }
+        })
+    }
+
+    private fun updateStartedActivitiesCount() {
+        Timber.d("updateStartedActivitiesCount: startedActivities size = ${startedActivities.size}")
+        Timber.d("updateStartedActivitiesCount: changingConfigurationActivities size = ${changingConfigurationActivities.size}")
+        startedActivitiesCount.value = startedActivities.size + changingConfigurationActivities.size
+    }
+
+    val hasStartedActivity = startedActivitiesCount.map { it > 0 }
+
+    private val foregroundServiceStarted = MutableStateFlow(false)
+
+    @MainThread
+    fun registerForegroundService(service: LifecycleService) {
+        Timber.d("registerForegroundService() called with: service = $service")
+        service.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onStart(owner: LifecycleOwner) {
+                Timber.d("onStart() called with: owner = $owner")
+                if (!foregroundServiceStarted.compareAndSet(expect = false, update = true)) {
+                    Timber.e("onStart: foreground service is already started")
+                }
+            }
+
+            override fun onStop(owner: LifecycleOwner) {
+                Timber.d("onStop() called with: owner = $owner")
+                if (!foregroundServiceStarted.compareAndSet(expect = true, update = false)) {
+                    Timber.e("onStart: foreground service is already stopped")
+                }
+            }
+        })
+    }
 
     val appInForeground = combine(hasStartedActivity, foregroundServiceStarted, Boolean::or)
-        .stateIn(scope, SharingStarted.Eagerly, false)
+        .stateIn(scope + Dispatchers.Unconfined, SharingStarted.Eagerly, false)
 
     init {
+        startedActivitiesCount.onEach { count ->
+            Timber.d("Started activities count = $count")
+        }.launchIn(scope + Dispatchers.Main)
+
+        foregroundServiceStarted.onEach { started ->
+            if (started) {
+                Timber.d("Foreground service is started")
+            } else {
+                Timber.d("Foreground service is stopped")
+            }
+        }.launchIn(scope + Dispatchers.Main)
+
         appInForeground
             .onEach { inForeground ->
                 if (inForeground) {
@@ -50,6 +121,6 @@ object AppForegroundTracker {
                     Timber.i("App is in background")
                 }
             }
-            .launchIn(scope)
+            .launchIn(scope + Dispatchers.Main)
     }
 }
