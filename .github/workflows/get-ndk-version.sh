db4c92fc2153badeccde46a0852d50bf93587509
Alexey Rochev <equeim@gmail.com>
2022-04-23 03:11:59 +0300

    Build environment

--- .github/workflows/get-ndk-version.sh
+++ .github/workflows/get-ndk-version.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 set -e -o pipefail
-toml -t gradle/libs.versions.toml -g 'versions.sdk-ndk' -j | jq -r '.["sdk-ndk"].value'
+versions="$1"
+if [[ -z $versions ]]; then
+    versions='gradle/libs.versions.toml'
+fi
+toml -t "$versions" -g 'versions.sdk-ndk' -j | jq -r '.["sdk-ndk"].value'
