1ae5cb5a1c98de8c2cae31e7d009accc3271e3ad
Alexey Rochev <equeim@gmail.com>
2024-05-09 02:33:58 +0300

    2.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
 
     defaultConfig {
         applicationId = "org.equeim.tremotesf"
-        versionCode = 4056
-        versionName = "2.10.1"
+        versionCode = 4058
+        versionName = "2.11.0"
     }
 
     if (keystoreProperties != null) {
