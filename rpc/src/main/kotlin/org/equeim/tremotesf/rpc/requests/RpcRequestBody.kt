08668a24d318de57a7c618344feb694009f87fe1
Alexey Rochev <equeim@gmail.com>
2025-05-10 00:15:08 +0300

    Encode body to Buffer instead of going through String

--- rpc/src/main/kotlin/org/equeim/tremotesf/rpc/requests/RpcRequestBody.kt
+++ rpc/src/main/kotlin/org/equeim/tremotesf/rpc/requests/RpcRequestBody.kt
@@ -4,36 +4,42 @@
 
 package org.equeim.tremotesf.rpc.requests
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.okio.encodeToBufferedSink
 import kotlinx.serialization.serializer
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody
+import okio.Buffer
 import okio.BufferedSink
 import java.io.IOException
 
 internal class RpcRequestBody internal constructor(
     internal val method: RpcMethod,
-    encodeToString: () -> String,
+    encodeToBuffer: () -> Buffer,
 ) : RequestBody() {
-    private val body: ByteArray by lazy {
+    private val body: Buffer by lazy {
         try {
-            encodeToString().encodeToByteArray()
+            encodeToBuffer()
         } catch (e: Exception) {
             throw IOException("Failed to serialize request body", e)
         }
     }
 
     override fun contentType(): MediaType = JSON_MEDIA_TYPE
-    override fun contentLength(): Long = body.size.toLong()
+    override fun contentLength(): Long = body.size
     override fun writeTo(sink: BufferedSink) {
-        sink.write(body)
+        sink.writeAll(body.peek())
     }
 }
 
+@OptIn(ExperimentalSerializationApi::class)
 private fun <T> RpcRequestBody(body: T, serializer: KSerializer<T>, method: RpcMethod, json: Json) =
-    RpcRequestBody(method) { json.encodeToString(serializer, body) }
+    RpcRequestBody(method) {
+        Buffer().apply { json.encodeToBufferedSink(serializer, body, this) }
+    }
 
 internal inline fun <reified RequestArgumentsT> RpcRequestBody(
     method: RpcMethod,
