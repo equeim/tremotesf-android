2ff4ad9fdc5a39428c9aa03d32c3fcee51b502cc
Alexey Rochev <equeim@gmail.com>
2025-05-22 23:13:10 +0300

    Add ComposeFragment

---
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/ComposeFragment.kt
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2017-2025 Alexey Rochev <equeim@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package org.equeim.tremotesf.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+
+abstract class ComposeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            val navController = findNavController()
+            setContent {
+                ApplicationTheme {
+                    Content(navController)
+                }
+            }
+        }
+    }
+
+    @Composable
+    abstract fun Content(navController: NavController)
+}
