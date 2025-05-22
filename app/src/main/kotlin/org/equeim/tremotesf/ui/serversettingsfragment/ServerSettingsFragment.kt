98e46a67a20117434210a8969bf1bcf225809491
Alexey Rochev <equeim@gmail.com>
2025-05-22 23:44:23 +0300

    Rewrite ServerSettingsFragment with Compose

--- app/src/main/kotlin/org/equeim/tremotesf/ui/serversettingsfragment/ServerSettingsFragment.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/serversettingsfragment/ServerSettingsFragment.kt
@@ -4,39 +4,88 @@
 
 package org.equeim.tremotesf.ui.serversettingsfragment
 
-import android.os.Bundle
-import android.widget.ArrayAdapter
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
 import org.equeim.tremotesf.R
-import org.equeim.tremotesf.databinding.ServerSettingsFragmentBinding
-import org.equeim.tremotesf.ui.NavigationFragment
+import org.equeim.tremotesf.ui.ComposeFragment
+import org.equeim.tremotesf.ui.ScreenPreview
+import org.equeim.tremotesf.ui.components.TremotesfTopAppBar
+import org.equeim.tremotesf.ui.utils.safeNavigate
 
-class ServerSettingsFragment : NavigationFragment(
-    R.layout.server_settings_fragment,
-    R.string.server_settings
-) {
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        ServerSettingsFragmentBinding.bind(requireView()).listView.apply {
-            adapter = ArrayAdapter(
-                requireContext(),
-                R.layout.server_settings_fragment_list_item,
-                resources.getStringArray(R.array.server_settings_items)
+class ServerSettingsFragment : ComposeFragment() {
+    @Composable
+    override fun Content(navController: NavController) {
+        ServerSettingsScreen(
+            navigateUp = navController::navigateUp,
+            navigate = navController::safeNavigate,
+        )
+    }
+}
+
+@Composable
+private fun ServerSettingsScreen(navigateUp: () -> Unit, navigate: (NavDirections) -> Unit) {
+    Scaffold(
+        topBar = {
+            TremotesfTopAppBar(
+                title = stringResource(R.string.server_settings),
+                navigateUp = navigateUp,
             )
-            divider = null
-            setSelector(android.R.color.transparent)
-            setOnItemClickListener { _, _, position, _ ->
-                val directions = when (position) {
-                    0 -> ServerSettingsFragmentDirections.toDownloadingFragment()
-                    1 -> ServerSettingsFragmentDirections.toSeedingFragment()
-                    2 -> ServerSettingsFragmentDirections.toQueueFragment()
-                    3 -> ServerSettingsFragmentDirections.toSpeedFragment()
-                    4 -> ServerSettingsFragmentDirections.toNetworkFragment()
-                    else -> null
-                }
-                if (directions != null) {
-                    navigate(directions)
-                }
+        }
+    ) { contentPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(contentPadding)
+        ) {
+            PageListItem(R.string.server_settings_downloading) {
+                navigate(ServerSettingsFragmentDirections.toDownloadingFragment())
+            }
+            PageListItem(R.string.server_settings_seeding) {
+                navigate(ServerSettingsFragmentDirections.toSeedingFragment())
+            }
+            PageListItem(R.string.server_settings_queue) {
+                navigate(ServerSettingsFragmentDirections.toQueueFragment())
+            }
+            PageListItem(R.string.server_settings_speed) {
+                navigate(ServerSettingsFragmentDirections.toSpeedFragment())
+            }
+            PageListItem(R.string.server_settings_network) {
+                navigate(ServerSettingsFragmentDirections.toNetworkFragment())
             }
         }
     }
+}
+
+@Composable
+private fun PageListItem(@StringRes title: Int, onClick: () -> Unit) {
+    ListItem(
+        headlineContent = {
+            Text(stringResource(title))
+        },
+        modifier = Modifier.clickable(onClick = onClick)
+    )
+}
+
+@Preview
+@Composable
+private fun ServerSettingsScreenPreview() = ScreenPreview {
+    ServerSettingsScreen(
+        navigateUp = {},
+        navigate = {}
+    )
 }
