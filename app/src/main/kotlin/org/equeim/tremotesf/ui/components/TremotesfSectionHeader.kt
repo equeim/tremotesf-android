8bbea5fdc341ea982bb6944e74b02781730f984e
Alexey Rochev <equeim@gmail.com>
2025-05-30 00:05:04 +0300

    Add TremotesfSectionHeader component

---
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/components/TremotesfSectionHeader.kt
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2017-2025 Alexey Rochev <equeim@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package org.equeim.tremotesf.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import org.equeim.tremotesf.R
+import org.equeim.tremotesf.ui.ComponentPreview
+import org.equeim.tremotesf.ui.Dimens
+
+@Composable
+fun TremotesfSectionHeader(@StringRes text: Int, modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(text),
+        style = MaterialTheme.typography.bodyMedium,
+        color = MaterialTheme.colorScheme.secondary,
+        modifier = modifier.semantics { heading() }
+    )
+}
+
+@Preview
+@Composable
+private fun TremotesfSectionHeaderPreview() = ComponentPreview {
+    TremotesfSectionHeader(R.string.limits)
+}
