9e805bad55f839aa625149aa5507c383ad6595f7
Alexey Rochev <equeim@gmail.com>
2025-06-17 23:46:04 +0300

    Add TremotesfLabelsEditor component

--- app/src/main/kotlin/org/equeim/tremotesf/ui/Icons.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/Icons.kt
@@ -196,3 +196,35 @@ val Icons.Filled.History: ImageVector
     }
 
 private var _history: ImageVector? = null
+
+val Icons.AutoMirrored.Outlined.Label: ImageVector
+    get() {
+        if (_label != null) {
+            return _label!!
+        }
+        _label = materialIcon(name = "AutoMirrored.Outlined.Label", autoMirror = true) {
+            materialPath {
+                moveTo(17.63f, 5.84f)
+                curveTo(17.27f, 5.33f, 16.67f, 5.0f, 16.0f, 5.0f)
+                lineTo(5.0f, 5.01f)
+                curveTo(3.9f, 5.01f, 3.0f, 5.9f, 3.0f, 7.0f)
+                verticalLineToRelative(10.0f)
+                curveToRelative(0.0f, 1.1f, 0.9f, 1.99f, 2.0f, 1.99f)
+                lineTo(16.0f, 19.0f)
+                curveToRelative(0.67f, 0.0f, 1.27f, -0.33f, 1.63f, -0.84f)
+                lineTo(22.0f, 12.0f)
+                lineToRelative(-4.37f, -6.16f)
+                close()
+                moveTo(16.0f, 17.0f)
+                horizontalLineTo(5.0f)
+                verticalLineTo(7.0f)
+                horizontalLineToRelative(11.0f)
+                lineToRelative(3.55f, 5.0f)
+                lineTo(16.0f, 17.0f)
+                close()
+            }
+        }
+        return _label!!
+    }
+
+private var _label: ImageVector? = null
---
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/components/TremotesfLabelsEditor.kt
@@ -0,0 +1,147 @@
+// SPDX-FileCopyrightText: 2017-2025 Alexey Rochev <equeim@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package org.equeim.tremotesf.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import org.equeim.tremotesf.R
+import org.equeim.tremotesf.ui.ComponentPreview
+import org.equeim.tremotesf.ui.Dimens
+import org.equeim.tremotesf.ui.Label
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TremotesfLabelsEditor(
+    enabledLabels: List<String>,
+    removeLabel: (String) -> Unit,
+    addLabel: (String) -> Unit,
+    allLabels: () -> List<String>,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier, verticalArrangement = Arrangement.spacedBy(Dimens.SpacingSmall)) {
+        EnabledLabelsList(labels = enabledLabels, removeLabel = removeLabel)
+
+        var expanded: Boolean by rememberSaveable { mutableStateOf(false) }
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = it },
+            //modifier = modifier.width(IntrinsicSize.Min)
+        ) {
+            var textEditValue: String by rememberSaveable { mutableStateOf("") }
+            OutlinedTextField(
+                value = textEditValue,
+                onValueChange = { textEditValue = it },
+                singleLine = true,
+                keyboardActions = KeyboardActions(
+                    onAny = {
+                        if (textEditValue.isNotBlank() && textEditValue !in enabledLabels) {
+                            addLabel(textEditValue)
+                            textEditValue = ""
+                        }
+                    }
+                ),
+                label = { Text(stringResource(R.string.new_label)) },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryEditable),
+            )
+            ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                for (label in allLabels()) {
+                    DropdownMenuItem(
+                        text = { Text(label) },
+                        leadingIcon = { Icon(Icons.AutoMirrored.Outlined.Label, contentDescription = label) },
+                        onClick = {
+                            if (label !in enabledLabels) {
+                                addLabel(label)
+                            }
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun EnabledLabelsList(
+    labels: List<String>,
+    removeLabel: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    FlowRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(Dimens.SpacingSmall),
+        verticalArrangement = Arrangement.spacedBy(Dimens.SpacingSmall)
+    ) {
+        for (label in labels) {
+            InputChip(
+                selected = false,
+                onClick = {},
+                label = {
+                    Box(modifier = Modifier.height(LocalMinimumInteractiveComponentSize.current)) {
+                        Text(label, modifier = Modifier.align(Alignment.Start + Alignment.CenterVertically))
+                    }
+                },
+                leadingIcon = {
+                    Icon(
+                        Icons.AutoMirrored.Outlined.Label,
+                        contentDescription = label,
+                        modifier = Modifier.padding(start = Dimens.SpacingSmall)
+                    )
+                },
+                trailingIcon = {
+                    TremotesfIconButtonWithTooltip(
+                        icon = Icons.Filled.Close,
+                        textId = R.string.remove
+                    ) { removeLabel(label) }
+                },
+                shape = MaterialTheme.shapes.medium
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() = ComponentPreview {
+    TremotesfLabelsEditor(
+        enabledLabels = remember { listOf("Lool", "Hmm", "NOPE") },
+        removeLabel = {},
+        addLabel = {},
+        allLabels = { listOf("42") }
+    )
+}
