cbf6512b9c6d2d520ee75e9e9ea25442d6f3db84
Alexey Rochev <equeim@gmail.com>
2025-07-11 02:08:46 +0300

    Highlight selected item in TremotesfComboBox

--- app/src/main/kotlin/org/equeim/tremotesf/ui/components/TremotesfComboBox.kt
+++ app/src/main/kotlin/org/equeim/tremotesf/ui/components/TremotesfComboBox.kt
@@ -5,14 +5,20 @@
 package org.equeim.tremotesf.ui.components
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +28,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import org.equeim.tremotesf.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,7 +51,9 @@ fun <T> TremotesfComboBox(
         OutlinedTextField(
             value = itemDisplayString(currentItem()),
             onValueChange = {},
-            modifier = Modifier.fillMaxWidth().menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable, enabled),
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable, enabled),
             enabled = enabled,
             readOnly = true,
             label = label.takeIf { it != 0 }?.let { { Text(stringResource(label)) } },
@@ -53,16 +62,52 @@ fun <T> TremotesfComboBox(
         )
         ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
             for (item in items) {
+                val selected = (item == currentItem())
                 DropdownMenuItem(
                     text = { Text(itemDisplayString(item)) },
-                    leadingIcon = leadingIcon?.let { { it(item) } },
+                    leadingIcon = when {
+                        leadingIcon != null -> {
+                            { leadingIcon(item) }
+                        }
+
+                        selected -> {
+                            { SelectedIcon() }
+                        }
+
+                        else -> null
+                    },
+                    trailingIcon = if (selected && leadingIcon != null) {
+                        { SelectedIcon() }
+                    } else {
+                        null
+                    },
                     onClick = {
                         updateCurrentItem(item)
                         expanded = false
                     },
                     contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    colors = if (selected) {
+                        val contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+                        MenuDefaults.itemColors(
+                            textColor = contentColor,
+                            leadingIconColor = contentColor,
+                            trailingIconColor = contentColor
+                        )
+                    } else {
+                        MenuDefaults.itemColors()
+                    },
+                    modifier = if (selected) {
+                        Modifier.background(MaterialTheme.colorScheme.secondaryContainer)
+                    } else {
+                        Modifier
+                    }
                 )
             }
         }
     }
+}
+
+@Composable
+private fun SelectedIcon() {
+    Icon(Icons.Filled.Check, contentDescription = stringResource(R.string.selected))
 }
