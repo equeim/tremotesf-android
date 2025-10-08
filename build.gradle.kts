93cee7f2363800ef5fa2d8edb0c81782a0150a9e
Alexey Rochev <equeim@gmail.com>
2025-10-09 00:08:29 +0300

    Replace com.github.ben-manes.versions plugin with com.deezer.caupain for dependency updates checking

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
+import com.deezer.caupain.model.StabilityLevelPolicy
+import com.deezer.caupain.plugin.DependenciesUpdateTask
+
 // SPDX-FileCopyrightText: 2017-2025 Alexey Rochev <equeim@gmail.com>
 //
 // SPDX-License-Identifier: CC0-1.0
-
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import java.util.Locale
 
 plugins {
     alias(libs.plugins.android.application) apply (false)
@@ -12,7 +12,7 @@ plugins {
     alias(libs.plugins.kotlin.plugin.parcelize) apply (false)
     alias(libs.plugins.kotlin.plugin.serialization) apply (false)
     alias(libs.plugins.androidx.navigation) apply (false)
-    alias(libs.plugins.gradle.versions)
+    alias(libs.plugins.deezer.caupain)
     alias(libs.plugins.tremotesf.common.settings)
 }
 
@@ -20,37 +20,6 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
-tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
-    gradleReleaseChannel = "current"
-    val channelProvider = VersionChannelProvider()
-    rejectVersionIf {
-        val currentChannel = channelProvider.getChannel(currentVersion)
-        val candidateChannel = channelProvider.getChannel(candidate.version)
-        candidateChannel < currentChannel
-    }
-}
-
-class VersionChannelProvider {
-    enum class Channel(private val keywords: List<String>) {
-        Alpha("alpha"),
-        Beta("beta"),
-        RC("rc"),
-        Stable("release", "final", "ga");
-
-        constructor(vararg keywords: String) : this(keywords.asList())
-
-        fun matches(versionLowercase: String): Boolean = keywords.any { versionLowercase.contains(it) }
-    }
-    private val channels = Channel.values()
-    private val stableVersionRegex = "^[0-9.]+$".toRegex()
-
-    fun getChannel(version: String): Channel {
-        val versionLowercase = version.lowercase(Locale.ROOT)
-        if (versionLowercase.matches(stableVersionRegex)) {
-            return Channel.Stable
-        }
-        val channelFromKeyword = channels.find { it.matches(versionLowercase) }
-        if (channelFromKeyword != null) return channelFromKeyword
-        throw RuntimeException("Failed to determine channel for version '$version'")
-    }
+tasks.withType<DependenciesUpdateTask> {
+    selectIf(StabilityLevelPolicy)
 }
